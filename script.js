341460d57254d3509c2396e937501b8259f1f24f
karuhi <h2401892@outlook.jp>
2020-06-13 15:31:34 +0900

    :+1: add changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var nowSlideNum = 0;
 var images = [
-  "https://i.picsum.photos/id/0/1200/628.jpg",
-  "https://i.picsum.photos/id/1/1200/628.jpg",
-  "https://i.picsum.photos/id/2/1200/628.jpg",
-  "https://i.picsum.photos/id/3/1200/628.jpg",
-  "https://i.picsum.photos/id/4/1200/628.jpg",
+  "https://picsum.photos/id/0/1200/628",
+  "https://picsum.photos/id/1/1200/628",
+  "https://picsum.photos/id/2/1200/628",
+  "https://picsum.photos/id/3/1200/628",
+  "https://picsum.photos/id/4/1200/628",
 ];
 var slideCounter;
 var imgLeft;
@@ -17,13 +17,15 @@ window.onload = function () {
 function initialize() {
   // 要素を読み込み
   slideCounter = document.getElementsByClassName("slide__counter");
-  imgLeft = document.getElementById("slide__container--left--image");
-  imgCenter = document.getElementById("slide__container--center--image");
-  imgRight = document.getElementById("slide__container--right--image");
+  imgLeft = document.getElementsByClassName("slide__container--left--image");
+  imgCenter = document.getElementsByClassName(
+    "slide__container--center--image"
+  );
+  imgRight = document.getElementsByClassName("slide__container--right--image");
   // 画像を挿入
-  imgLeft.src = images[0];
-  imgCenter.src = images[1];
-  imgRight.src = images[2];
+  imgLeft[0].src = images[0];
+  imgCenter[0].src = images[1];
+  imgRight[0].src = images[2];
   // スライドのページネーションを表示
   for (var i = 0; i < images.length; i++) {
     var span = document.createElement("span");
