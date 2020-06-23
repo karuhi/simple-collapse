09c0e9ee123a5e33652f62379787dfdb5a29ba65
karuhi <h2401892@outlook.jp>
2020-06-23 23:33:03 +0900

    :+1: fixed! close #1

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function toggleShowStatus(element) {
-  console.log("click");
   var next = element.nextElementSibling;
   next.classList.toggle("active");
+  var last = element.lastElementChild;
+  last.classList.toggle("active");
 }
